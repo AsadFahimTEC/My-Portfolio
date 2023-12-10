7ee4c5bf018defda29088ff8b1ab4f41d4b45a00
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2023-12-11 00:56:36 +0600

    my projects section design finish properly

--- src/TourStory/TourStory.jsx
+++ src/TourStory/TourStory.jsx
@@ -1,96 +1,61 @@
-
-
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { FacebookShareButton, FacebookIcon } from 'react-share';
-
-// Mock data for tourist stories
-const storiesData = [
-  {
-    id: 1,
-    title: 'JiveDiscover',
-    content: 'Unleash the wanderlust within you with JiveDiscover.Explore diverse cultures, scenic wonders, and thrilling experiences across the globe. Your journey begins here.',
-  },
-  {
-    id: 2,
-    title: 'JiveVoyages',
-    content: 'Embark on unforgettable journeys with JiveVoyages. Immerse yourself in extraordinary destinations, curated for those who seek more from travel. Let your adventures unfold.',
-  },
-  {
-    id: 3,
-    title: 'JiveDestiny',
-    content: 'JiveDestiny invites you to chart your own course. Enjoy tailor-made tours that blend adventure, culture, and relaxation. Your destiny is to explore, and we are here to guide you.',
-  },
-  {
-    id: 4,
-    title: 'JiveOdyssey',
-    content: 'Join the JiveOdyssey and navigate a world of wonders. From epic landscapes to hidden gems, each journey is a chapter in your personal odyssey. Where will your story take you.',
-  },
-];
-
-// TouristStory component to display each story
-const TouristStory = ({ story, onViewDetails}) => {
+import './tourStory.css';
+const TourStory = () => {
   return (
-    <div style={{ border: '1px solid #ccc', padding: '10px', margin: '10px' }}>
-      <h3 className='text-xl text-red-500'>{story.title}</h3>
-      <Link to={`/services/${story.id}`}>
-      <p className='text-green-600'>{story.content}</p></Link>
-      <div className='flex gap-4 mt-3'>
-      <button className='btn btn-xs btn-secondary' onClick={() => onViewDetails(story.id)}>Share Story</button>
-      <Link to={`/services/${story.id}`}>
-      <button className='btn btn-xs btn-primary'>More Details</button>
-      </Link>
+    <div className="mt-4">
+      <h3 className='text-center text-blue-600 text-4xl'>My Projects</h3>
+      <div className="container">
+        <div className="parent">
+          <div className="card">
+            <div className="content-box">
+              <h1 className="card-title">JourneyJive</h1>
+              <p className="card-content">
+              My Project Name is JourneyJive. It is a Tourist Guide projects for people with 6 popular packages such like as JiveDiscover, JiveVoyages, JiveDestiny, JiveOdyssey, JiveWanderlust, JiveHorizons etc.
+              </p>
+              <a href='https://peoplepro-af1ed.web.app' target= 'blank'  className="see-more">Live Website</a>
+            </div>
+            <div className="date-box">
+              <span className="month">Project</span>
+              <span className="date">01</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="parent card2">
+          <div className="card22">
+            <div className="content-box">
+              <h1 className="card-title">BookStore</h1>
+              <p className="card-content">
+              My Project Name is BookStore. It is a book services such like as Book Recommendation Service, Literary Events Calendar, Book Club Subscription, Bookstore Loyalty Program, etc.
+              </p>
+              <a href='https://654cd7a74cf70141250f52fc--cosmic-crisp-16b882.netlify.app' target= 'blank'  className="see-more">Live Website</a>
+            </div>
+            <div className="date-box">
+              <span className="month">Project</span>
+              <span className="date">02</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="parent card3">
+          <div className="card33">
+            <div className="content-box">
+              <h1 className="card-title">Drinko</h1>
+              <p className="card-content">
+              My Project Name is Drinko. It is a food and beverage projects for oeople with 6 brands category such like as Coca-Cola, McDonald's, Starbucks, PepsiCo, Nestlé, Kellogg's, etc.In this project at first it has a navbar.
+              </p>
+              <a href='https://brand-shop-e0506.web.app' target= 'blank' className="see-more">Live Website</a>
+            </div>
+            <div className="date-box">
+              <span className="month">Project</span>
+              <span className="date">03</span>
+            </div>
+          </div>
+        </div>
+
+       
       </div>
-      
     </div>
   );
 };
 
-// StoryDetail component to display the details of a selected story
-const StoryDetail = ({ story, onShare }) => {
-  return (
-    <div style={{ border: '1px solid #ccc', padding: '10px', margin: '10px' }}>
-      <h3 className='text-xl text-red-500'>{story.title}</h3>
-      <p className='text-green-600'>{story.content}</p>
-      <FacebookShareButton url={window.location.href} quote={story.title}>
-        <FacebookIcon className='mt-2' size={32} round />
-        Share on Facebook
-      </FacebookShareButton>
-      <button onClick={onShare}></button>
-     
-    </div>
-  );
-};
-
-// Main component that renders the list of tourist stories
-const TouristStories = () => {
-  const [selectedStory, setSelectedStory] = useState(null);
-
-  const onViewDetails = (storyId) => {
-    const selected = storiesData.find((story) => story.id === storyId);
-    setSelectedStory(selected);
-  };
-
-  const onShare = () => {
-    // Custom share logic can be implemented here
-    // alert('Custom Share Logic');
-  };
-
-  return (
-    <div>
-      <h1 className='mt-2 text-2xl text-center text-blue-700'>Tourist Stories</h1>
-      {storiesData.map((story) => (
-        <TouristStory key={story.id} story={story} onViewDetails={onViewDetails} />
-      ))}
-      {selectedStory && <StoryDetail story={selectedStory} onShare={onShare} />}
-      <Link to="/allstory">
-      <button
-            className="ml-10 bg-[green] hover:bg-[blue] font-avenir text-[white] rounded-lg p-1">
-            All Stories
-          </button>
-          </Link>
-    </div>
-  );
-};
-
-export default TouristStories;
+export default TourStory;
