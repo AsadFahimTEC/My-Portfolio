c6259507c521f08a23b7a26e2f2726abbc275017
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2023-12-11 08:38:28 +0600

    experience section design finished properly

--- src/TourStory/TourStory.jsx
+++ src/TourStory/TourStory.jsx
@@ -11,7 +11,10 @@ const TourStory = () => {
               <p className="card-content">
               My Project Name is JourneyJive. It is a Tourist Guide projects for people with 6 popular packages such like as JiveDiscover, JiveVoyages, JiveDestiny, JiveOdyssey, JiveWanderlust, JiveHorizons etc.
               </p>
+              <div className="flex gap-3">
               <a href='https://peoplepro-af1ed.web.app' target= 'blank'  className="see-more">Live Website</a>
+              <a href='https://github.com/AsadFahimTEC/b8a12-client-side' target= 'blank'  className="see-more2">View Code</a>
+              </div>
             </div>
             <div className="date-box">
               <span className="month">Project</span>
@@ -27,7 +30,10 @@ const TourStory = () => {
               <p className="card-content">
               My Project Name is BookStore. It is a book services such like as Book Recommendation Service, Literary Events Calendar, Book Club Subscription, Bookstore Loyalty Program, etc.
               </p>
-              <a href='https://654cd7a74cf70141250f52fc--cosmic-crisp-16b882.netlify.app' target= 'blank'  className="see-more">Live Website</a>
+             <div className='flex gap-3'>
+             <a href='https://654cd7a74cf70141250f52fc--cosmic-crisp-16b882.netlify.app' target= 'blank'  className="see-more">Live Website</a>
+              <a href='https://github.com/AsadFahimTEC/b8a11-client-side' target= 'blank'  className="see-more3">View Code</a>
+             </div>
             </div>
             <div className="date-box">
               <span className="month">Project</span>
@@ -43,7 +49,10 @@ const TourStory = () => {
               <p className="card-content">
               My Project Name is Drinko. It is a food and beverage projects for oeople with 6 brands category such like as Coca-Cola, McDonald's, Starbucks, PepsiCo, Nestlé, Kellogg's, etc.In this project at first it has a navbar.
               </p>
+              <div className='flex gap-3'>
               <a href='https://brand-shop-e0506.web.app' target= 'blank' className="see-more">Live Website</a>
+              <a href='https://github.com/AsadFahimTEC/b8a10-brandshop-client-side' target= 'blank'  className="see-more4">View Code</a>
+              </div>
             </div>
             <div className="date-box">
               <span className="month">Project</span>
