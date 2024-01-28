a345f83f48ce7ae9a3b77741a5fac9372c231132
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2024-01-28 14:35:27 +0600

    skills section design change properly

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,8 +3,9 @@ import Banner from "../Banner/Banner";
 import Contact from "../ContactUs/Contact";
 import ContactUs from "../ContactUs/ContactUs";
 import Education from "../Education/Education";
+import Skills from "../Skills/Skills";
 // import Services from "../Services/Services";
-import Tabs from "../Tabs/Tabs";
+// import Tabs from "../Tabs/Tabs";
 import TouristStories from "../TourStory/TourStory";
 // import Type from "../Type/Type";
 
@@ -14,7 +15,8 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
-            <Tabs></Tabs>
+            <Skills></Skills>
+            {/* <Tabs></Tabs> */}
             <TouristStories></TouristStories>
             <Contact></Contact>
             {/* <Type></Type> */}
---
+++ src/Skills/Skills.jsx
@@ -0,0 +1,134 @@
+import './Skills.css';
+const Skills = () => {
+  return (
+    <div className="mt-2">
+      <h2 className="text-center text-3xl text-purple-300 hover:text-red-400">
+        My Skills
+      </h2>
+      <div className="marquee mt-2">
+        <div className="marquee-content">
+          <div className="marquee-item">
+            <img
+              src="https://i.ibb.co/BwZ0F69/png-transparent-logo-html-html5-thumbnail-removebg-preview.png"
+              alt=""
+            />
+          </div>
+
+          <div className="marquee-item">
+            <img
+              src="https://i.ibb.co/Lvbw8KV/png-clipart-html-css3-cascading-style-sheets-logo-markup-language-digital-agency-miscellaneous-blue.png"
+              alt=""
+            />
+          </div>
+
+          <div className="marquee-item">
+            <img
+              src="https://i.ibb.co/cthQ5yz/javascript-1024x341-removebg-preview.png"
+              alt=""
+            />
+          </div>
+          <div className="marquee-item">
+            <img
+              src="https://i.ibb.co/Xk8P0rn/download-removebg-preview.png"
+              alt=""
+            />
+          </div>
+
+          <div className="marquee-item">
+            <img
+              src="https://i.ibb.co/RBf741X/React-icon-svg-removebg-preview.png"
+              alt=""
+            />
+          </div>
+
+          {/* <div className="marquee-item">
+            <img
+              src="https://i.ibb.co/PZ1Cpf5/1-do-Ag1-f-MQKWFoub-6gw-Ui-Q-removebg-preview.png"
+              alt=""
+            />
+          </div> */}
+
+          <div className="marquee-item">
+            <img
+              src="https://i.ibb.co/TPtC5jD/1-t-OI6-UC5-Ea-S2f-PIt-Ces-I-AQ-removebg-preview.png"
+              alt=""
+            />
+          </div>
+
+          <div className="marquee-item">
+            <img
+              src="https://i.ibb.co/ZTq2R4Z/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo-removebg-preview.png"
+              alt=""
+            />
+          </div>
+
+          <div className="marquee-item">
+            <img
+              src="https://i.ibb.co/BwZ0F69/png-transparent-logo-html-html5-thumbnail-removebg-preview.png"
+              alt=""
+            />
+          </div>
+
+          <div className="marquee-item">
+            <img
+              src="https://i.ibb.co/Lvbw8KV/png-clipart-html-css3-cascading-style-sheets-logo-markup-language-digital-agency-miscellaneous-blue.png"
+              alt=""
+            />
+          </div>
+
+          <div className="marquee-item">
+            <img
+              src="https://i.ibb.co/cthQ5yz/javascript-1024x341-removebg-preview.png"
+              alt=""
+            />
+          </div>
+
+          <div className="marquee-item">
+            <img
+              src="https://i.ibb.co/Xk8P0rn/download-removebg-preview.png"
+              alt=""
+            />
+          </div>
+
+          <div className="marquee-item">
+            <img
+              src="https://i.ibb.co/RBf741X/React-icon-svg-removebg-preview.png"
+              alt=""
+            />
+          </div>
+
+          {/* <div className="marquee-item">
+            <img
+              src="https://i.ibb.co/mqbw7S1/44940-removebg-preview.png"
+              alt=""
+            />
+          </div> */}
+
+          <div className="marquee-item">
+            <img
+              src="https://i.ibb.co/PZ1Cpf5/1-do-Ag1-f-MQKWFoub-6gw-Ui-Q-removebg-preview.png"
+              alt=""
+            />
+          </div>
+
+          <div className="marquee-item">
+            <img
+              src="https://i.ibb.co/TPtC5jD/1-t-OI6-UC5-Ea-S2f-PIt-Ces-I-AQ-removebg-preview.png"
+              alt=""
+            />
+          </div>
+
+          <div className="marquee-item">
+            <img
+              src="https://i.ibb.co/ZTq2R4Z/png-transparent-express-js-node-js-javascript-mongodb-node-js-text-trademark-logo-removebg-preview.png"
+              alt=""
+            />
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
